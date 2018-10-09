65157040ea84fb55f03ff9ca8078dea11462c577
vagrant <vagrant@homestead>
2018-10-09 06:45:29 +0000

    商品模型

--- config/app.php
+++ config/app.php
@@ -179,6 +179,8 @@ return [
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
 
+        // IDE helper
+        Barryvdh\LaravelIdeHelper\IdeHelperServiceProvider::class
     ],
 
     /*
