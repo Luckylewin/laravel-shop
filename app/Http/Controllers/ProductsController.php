3f4ce67189b0a67b24caec801181323a1a4f8dcc
vagrant <vagrant@homestead>
2018-10-09 09:09:54 +0000

    用户界面 - 商品筛选,排序

--- app/Http/Controllers/ProductsController.php
+++ app/Http/Controllers/ProductsController.php
@@ -10,9 +10,38 @@ class ProductsController extends Controller
     //
     public function index(Request $request)
     {
-        $products = Product::query()->where('on_sale', true)
-                                    ->paginate(16);
+        $queryBuilder = Product::query()->where('on_sale', true);
 
-        return view('products.index', ['products' => $products]);
+        if ($search = $request->input('search', '')) {
+            $like = '%'.$search.'%';
+            // 模糊搜索商品标题，商品详情，sku标题，sku描述
+            $queryBuilder->where(function($query) use ($like) {
+                 $query->where('title', 'like', $like)
+                       ->orWhere('description', 'like', $like)
+                       ->orWhereHas('skus', function ($query) use ($like) {
+                           $query->where('title', 'like', $like)
+                                 ->orWhere('description', 'like', $like);
+                       });
+            });
+        }
+
+        if ($order = $request->input('order', '')) {
+            if (preg_match('/^(.+)_(asc|desc)$/', $order, $m)) {
+                // 如果字符串的开头是这 3 个字符串之一，说明是一个合法的排序值
+                if (in_array($m[1], ['price', 'sold_count', 'rating'])) {
+                    $queryBuilder->orderBy($m[1], $m[2]);
+                }
+            }
+        }
+
+        $products = $queryBuilder->paginate(16);
+
+        return view('products.index', [
+            'products' => $products,
+            'filters' => [
+                'search' => $search,
+                'order' => $order
+            ]
+        ]);
     }
 }
