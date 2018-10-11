86caf3deac9eb6682ac562dbbd93092b18b2f6d2
lychee <876505905@qq.com>
2018-10-11 09:25:37 +0000

    关闭未支付的订单

--- app/Http/Controllers/OrdersController.php
+++ app/Http/Controllers/OrdersController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Exceptions\InvalidRequestException;
 use App\Http\Requests\OrderRequest;
+use App\Jobs\CloseOrder;
 use App\Models\Order;
 use App\Models\OrderItem;
 use App\Models\ProductSku;
@@ -70,7 +71,13 @@ class OrdersController extends Controller
             // 将下单的商品从购物车中移除
             $skuIds = collect($request->input('items'))->pluck('sku_id');
             $user->cartItems()->whereIn('product_sku_id', $skuIds)->delete();
+
+            return $order;
         });
+
+
+        // 把订单加入延时任务
+        $this->dispatch(new CloseOrder($order, config('app.order_ttl')));
 
         return $order;
     }
---
+++ app/Jobs/CloseOrder.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace App\Jobs;
+
+use App\Models\Order;
+use App\Models\ProductSku;
+use Illuminate\Bus\Queueable;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Bus\Dispatchable;
+
+/**
+ * 关闭订单延时任务
+ * Class CloseOrder
+ * @package App\Jobs
+ */
+class CloseOrder implements ShouldQueue
+{
+    use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
+
+    /**
+     * @var Order
+     */
+    protected $order;
+
+    /**
+     * CloseOrder constructor.
+     * @param Order $order
+     * @param $delay
+     */
+    public function __construct(Order $order, $delay)
+    {
+        $this->order = $order;
+        $this->delay($delay);
+    }
+
+
+    public function handle()
+    {
+        if ($this->order->paid_at) {
+            return;
+        }
+
+        // 通过事务执行sql
+        \DB::transaction(function() {
+            // 将订单的 closed 字段标记为 true, 即关闭订单
+            $this->order->update(['closed' => true]);
+            // 循环遍历订单中的sku 把订单中的数量放回库存
+            foreach ($this->order->items as $item) {
+                $item->productSku->increaseStock($item->amount);
+            }
+        });
+    }
+}
