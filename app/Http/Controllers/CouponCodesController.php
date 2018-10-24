5fe2b3c76dbf0e2ad4094d35ed3e556d2e73b7d9
lychee <876505905@qq.com>
2018-10-24 08:24:59 +0000

    优化优惠券模块

--- app/Http/Controllers/CouponCodesController.php
+++ app/Http/Controllers/CouponCodesController.php
@@ -4,19 +4,20 @@ namespace App\Http\Controllers;
 
 use App\Exceptions\CouponCodeUnavailableException;
 use App\Models\CouponCode;
+use Illuminate\Http\Request;
 
 
 class CouponCodesController extends Controller
 {
 
-    public function show($code)
+    public function show(Request $request, $code)
     {
         // 判断优惠券 是否存在
         if (!$record = CouponCode::query()->where('code', $code)->first()) {
            throw new CouponCodeUnavailableException('优惠券不存在');
         }
 
-        $record->checkAvailable();
+        $record->checkAvailable($request->user());
 
         return $record;
     }
